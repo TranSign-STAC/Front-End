d448cecc115bca9679f8d22c80893d069efe55f5
GangbacOl <gangyeonjo336@gmail.com>
2021-01-20 16:13:08 +0900

    Add variables.ts for using breakpoint, color, font size

---
+++ src/style/variables.ts
@@ -0,0 +1,21 @@
+// Breakpoints
+export const breakpointMobile: number = 425;
+export const breakpointTablet: number = 768;
+export const breakpointDesktopM: number = 1280;
+export const breakpointDesktopL: number = 1366;
+
+// Font size
+export const largeTitle: number = 45;
+export const title1: number = 35;
+export const title2: number = 25;
+export const title3: number = 20;
+export const subTitle: number = 19;
+export const paragraph: number = 16;
+export const smallText: number = 12;
+
+// color
+export const PURPLE: string = '#5446F6';
+export const BLACK: string = '#000000';
+export const WHITE: string = '#ffffff';
+export const GREEN: string = '#50ee8f';
+export const RED: string = '#fa6666';
