a0510f4993d571de1b64c0bec581bc2ee15f884b
GangbacOl <gangyeonjo336@gmail.com>
2021-01-21 22:09:48 +0900

    Update next config for using font

--- next.config.js
+++ next.config.js
@@ -1,13 +1,27 @@
 module.exports = {
-  webpack(config) {
-    config.module.rules.push({
-      test: /\.svg$/,
-      issuer: {
-        test: /\.(js|ts|jsx|tsx)x?$/,
-      },
-      use: ['@svgr/webpack'],
-    });
-
-    return config;
-  },
+    webpack(config) {
+        config.module.rules.push(
+            {
+                test: /\.svg$/,
+                issuer: {
+                    test: /\.(js|ts|jsx|tsx)x?$/,
+                },
+                use: ['@svgr/webpack'],
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            limit: 10000,
+                            fallback: 'file-loader',
+                            name: 'fonts/[name].[ext]',
+                        },
+                    },
+                ],
+            }
+        );
+        return config;
+    },
 };
