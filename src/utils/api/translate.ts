6497fb3946ebeb4e856c858cb43dc9c6386597dc
GangbacOl <gangyeonjo336@gmail.com>
2021-02-26 21:58:19 +0900

    Update api request logic

--- src/utils/api/translate.ts
+++ src/utils/api/translate.ts
@@ -1,9 +1,14 @@
 import { TRANSLATE_API } from '../url';
 import axios from 'axios';
+import { parseCookies } from 'nookies';
 
-type Data = {
+type DataType = {
     uuid: string;
     text: string;
 };
 
-export const translateTextToSign = async (data: Data) => await axios.post(TRANSLATE_API, data);
+export const translateTextToSign = async (data: DataType) => {
+    const uuid = parseCookies().uuid;
+    data.uuid = uuid;
+    return await axios.post(TRANSLATE_API, JSON.stringify(data));
+};
