6eee0300ddfafa25f0145f2007edf6e9d71a4ddd
GangbacOl <gangyeonjo336@gmail.com>
2021-03-09 14:48:26 +0900

    Add record audio feature

--- src/components/Button/VoiceRecBtn.tsx
+++ src/components/Button/VoiceRecBtn.tsx
@@ -1,13 +1,52 @@
-import React, { useState } from 'react';
+import React, { useState, useCallback, memo } from 'react';
 import styled, { css } from 'styled-components';
 
 import Mic from '../../../public/images/fill/mic.svg';
 import MicWhite from '../../../public/images/white/mic.svg';
 
 import * as theme from '../../style/theme';
+import * as mixin from '../../style/mixin';
+
+const recordAudio = () =>
+    new Promise(async (resolve) => {
+        if ('mediaDevices' in navigator && navigator.mediaDevices.getUserMedia) {
+            const stream: any = await navigator.mediaDevices.getUserMedia({ audio: true });
+            const mediaRecorder = new MediaRecorder(stream);
+            const audioChunks: Blob[] = [];
+
+            mediaRecorder.addEventListener('dataavailable', (event: any) => {
+                audioChunks.push(event.data);
+            });
+
+            const start = () => {
+                console.log(mediaRecorder.state);
+                mediaRecorder.start();
+                console.log(mediaRecorder.state);
+            };
+
+            const stop = () =>
+                new Promise((resolve) => {
+                    mediaRecorder.addEventListener('stop', () => {
+                        const audioBlob = new Blob(audioChunks, { type: 'audio/wav; codecs=0' });
+                        resolve({ audioBlob });
+                    });
+                    console.log(mediaRecorder.state);
+                    if (mediaRecorder.state !== 'inactive') mediaRecorder.stop();
+                    console.log(mediaRecorder.state);
+                });
+
+            resolve({ start, stop });
+        } else {
+            alert('마이크를 찾을 수 없습니다.');
+        }
+    });
 
 type StyleProps = {
     active: boolean;
+};
+
+type Props = {
+    isMobileTablet: boolean;
 };
 
 const Container = styled.div<StyleProps>`
@@ -27,10 +66,15 @@ const Container = styled.div<StyleProps>`
                   background: ${theme.WHITE};
                   box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
               `};
+    ${mixin.mobile(`
+        margin-bottom: 24px;
+    `)}
+    ${mixin.tablet(`
+        margin-right: 32px;
+    `)}
 `;
 
-const Button = styled.button<StyleProps>`
-    all: unset;
+const ButtonWrap = styled.div<StyleProps>`
     width: 100%;
     height: 100%;
     display: flex;
@@ -46,36 +90,61 @@ const Button = styled.button<StyleProps>`
     }
 `;
 
+const Button = styled.button`
+    all: unset;
+    width: 100%;
+    height: 100%;
+    border-radius: 40px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
 const Span = styled.span`
     width: 96px;
     height: 24px;
     display: flex;
     justify-content: center;
     align-items: center;
+    margin-left: 10px;
 `;
 
-const VoiceRecBtn: React.FC = () => {
+const VoiceRecBtn = ({ isMobileTablet }: Props) => {
     const [isClicked, setIsClicked] = useState<boolean>(false);
+    let recorder: any = null;
 
-    const handleClick = () => setIsClicked(true);
+    const handleStart = useCallback(async () => {
+        if (isMobileTablet) {
+            alert('해당 기능은 앱을 통해서 사용할 수 있습니다.');
+            return;
+        }
+        setIsClicked(true);
+        recorder = await recordAudio();
+        recorder.start();
+    }, [recorder]);
+    const handleStop = useCallback(async () => {
+        setIsClicked(false);
+        const recordedBlob = await recorder.stop();
+        console.log(recordedBlob);
+    }, [recorder]);
 
     return (
         <Container active={isClicked}>
-            <Button onClick={handleClick} active={isClicked}>
+            <ButtonWrap active={isClicked}>
                 {isClicked ? (
-                    <>
+                    <Button onClick={handleStop}>
                         <MicWhite />
                         <Span>음성 번역 중..</Span>
-                    </>
+                    </Button>
                 ) : (
-                    <>
+                    <Button onClick={handleStart}>
                         <Mic />
                         <Span>음성 번역</Span>
-                    </>
+                    </Button>
                 )}
-            </Button>
+            </ButtonWrap>
         </Container>
     );
 };
 
-export default VoiceRecBtn;
+export default memo(VoiceRecBtn);
