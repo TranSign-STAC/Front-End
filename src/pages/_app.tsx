6f6d4fc5f2e14c2ccf031cf8a187f974e5a6d0e5
GangbacOl <gangyeonjo336@gmail.com>
2021-03-09 16:00:41 +0900

    Add getInitialProps for setting cookie

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,23 @@
+import { AppProps, AppContext } from 'next/app';
 import withReduxSaga from 'next-redux-saga';
+import { v4 as uuidv4 } from 'uuid';
+import nookies from 'nookies';
 
 import { GlobalStyle } from '../style/globalStyle';
-
 import wrapper from '../store/configureStore';
 
-const TranSign = ({ Component, pageProps }: any) => {
+const TranSign = ({ Component, pageProps }: AppProps) => {
     return (
         <>
             <GlobalStyle />
             <Component {...pageProps} />
         </>
     );
+};
+
+TranSign.getInitialProps = async ({ ctx }: AppContext) => {
+    const uuid = nookies.get(ctx).uuid;
+    if (!uuid) nookies.set(ctx, 'uuid', uuidv4());
 };
 
 export default wrapper.withRedux(withReduxSaga(TranSign));
