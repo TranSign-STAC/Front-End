7cf47d351f9bb31790db27579bbbdfb0f14a26a0
GangbacOl <gangyeonjo336@gmail.com>
2021-03-09 14:55:11 +0900

    Add media query mixin

--- src/components/Modal/DeleteModal.tsx
+++ src/components/Modal/DeleteModal.tsx
@@ -4,63 +4,116 @@ import styled from 'styled-components';
 import DeleteBtn from '../Button/DeleteBtn';
 
 import Close from '../../../public/images/outline/close.svg';
-import Trash from '../../../public/images/white/trash.svg';
+import TrashWhite from '../../../public/images/white/trash.svg';
+import Trash from '../../../public/images/fill/trash.svg';
 
 import * as theme from '../../style/theme';
+import * as mixin from '../../style/mixin';
 
 const Container = styled.div`
     position: relative;
+    z-index: 9999;
     width: 480px;
-    height: 325px;
+    height: 332px;
+    border-radius: 4px;
     display: flex;
     flex-direction: column;
     align-items: center;
     justify-items: center;
-    border-radius: 4px;
+    background: ${theme.WHITE};
+    ${mixin.mobile(`
+        width: 311px;
+        height: 191px;
+        border-radius: 8px;
+    `)}
 `;
 
-const CloseWrapper = styled.div`
+const CloseWrap = styled.div`
     position: absolute;
     top: 10px;
     right: 10px;
 `;
 
-const TrashWrapper = styled.div`
-    margin: 25px auto;
-    width: 72px;
-    height: 72px;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    background: ${theme.RED};
-    border-radius: 100%;
+const TrashWrap = styled.div`
+    ${mixin.mobile(`
+        margin: 25.5px 0 9.5px 0;
+    `)}
+    ${mixin.tablet(`
+        margin: 24px auto;
+        width: 72px;
+        height: 72px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: ${theme.RED};
+        border-radius: 100%;
+    `)}
+    ${mixin.desktop(`
+        margin: 25px auto;
+        width: 72px;
+        height: 72px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        background: ${theme.RED};
+        border-radius: 100%;
+    `)}
 `;
 
 const Title = styled.h2`
     color: ${theme.RED};
-    font-size: 24px;
     font-weight: 700;
-    line-height: 24px;
     text-align: center;
-    margin-bottom: 30px;
+    ${mixin.mobile(`
+        font-size: 18px;
+        line-height: 27px;
+        margin-bottom: 6px;
+    `)}
+    ${mixin.tablet(`
+        font-size: 24px;
+        line-height: 36px;
+        margin-bottom: 16px;
+    `)}
+    ${mixin.desktop(`
+        font-size: 24px;
+        line-height: 35.5px;
+        margin-bottom: 16px;
+    `)}
 `;
 
 const Describe = styled.p`
-    font-size: 16px;
     font-weight: 300;
     text-align: center;
-    margin-bottom: 40px;
+    ${mixin.mobile(`
+        font-size: 12px;
+        line-height: 18px;
+        margin-bottom: 16px;
+    `)}
+    ${mixin.tablet(`
+        font-size: 16px;
+        line-height: 24px;
+        margin-bottom: 40px;
+    `)}
+    ${mixin.desktop(`font-size: 16px;
+        line-height: 24px;
+        margin-bottom: 40px;
+    `)}
 `;
 
-const DeleteModal: React.FC = () => {
+type Props = {
+    isMobile: Boolean;
+    handleClick: Function;
+};
+
+const DeleteModal = ({ isMobile, handleClick }: Props) => {
     return (
         <Container>
-            <CloseWrapper>
-                <Close />
-            </CloseWrapper>
-            <TrashWrapper>
-                <Trash />
-            </TrashWrapper>
+            {!isMobile && (
+                <CloseWrap onClick={handleClick}>
+                    <Close />
+                </CloseWrap>
+            )}
+            <TrashWrap>{isMobile ? <Trash /> : <TrashWhite />}</TrashWrap>
             <Title>번역 기록 삭제</Title>
             <Describe>
                 삭제한 번역 기록은 복구할 수 없습니다. <br />
--- src/components/Modal/TranslatingModal.tsx
+++ src/components/Modal/TranslatingModal.tsx
@@ -5,9 +5,16 @@ import Close from '../../../public/images/outline/close.svg';
 import Translation from '../../../public/images/white/translation.svg';
 
 import * as theme from '../../style/theme';
+import * as mixin from '../../style/mixin';
+
+type Props = {
+    isMobile: boolean;
+    setIsTranslating: Function;
+};
 
 const Container = styled.div`
     position: relative;
+    z-index: 9999;
     width: 480px;
     height: 325px;
     display: flex;
@@ -15,15 +22,21 @@ const Container = styled.div`
     align-items: center;
     justify-items: center;
     border-radius: 4px;
+    background: ${theme.WHITE};
+    ${mixin.mobile(`
+        width: 311px;
+        height: 280px;
+        border-radius: 8px;
+    `)}
 `;
 
-const CloseWrapper = styled.div`
+const CloseWrap = styled.div`
     position: absolute;
     top: 10px;
     right: 10px;
 `;
 
-const TranslationWrapper = styled.div`
+const TranslationWrap = styled.div`
     margin: 35px auto;
     width: 192px;
     height: 192px;
@@ -32,6 +45,10 @@ const TranslationWrapper = styled.div`
     align-items: center;
     background: ${theme.PURPLE};
     border-radius: 100%;
+    ${mixin.mobile(`
+        width: 128px;
+        height: 128px;
+    `)}
 `;
 
 const Describe = styled.p`
@@ -39,19 +56,36 @@ const Describe = styled.p`
     font-weight: 700;
     text-align: center;
     margin-bottom: 40px;
-    letter-spacing: 0.5px;
+    ${mixin.mobile(`
+        margin-bottom: 16px;
+    `)}
 `;
 
-const TranslatingModal: React.FC = () => {
+const CancelButton = styled.button`
+    width: 100%;
+    height: 48px;
+    border: none;
+    border-bottom-left-radius: 8px;
+    border-bottom-right-radius: 8px;
+    background: ${theme.PURPLE};
+    color: ${theme.WHITE};
+    text-align: center;
+`;
+
+const TranslatingModal = ({ isMobile, setIsTranslating }: Props) => {
+    const handleClick = () => setIsTranslating(false);
     return (
         <Container>
-            <CloseWrapper>
-                <Close />
-            </CloseWrapper>
-            <TranslationWrapper>
+            {!isMobile && (
+                <CloseWrap onClick={handleClick}>
+                    <Close />
+                </CloseWrap>
+            )}
+            <TranslationWrap>
                 <Translation />
-            </TranslationWrapper>
-            <Describe>번역중입니다...</Describe>
+            </TranslationWrap>
+            <Describe>번역중...</Describe>
+            {isMobile && <CancelButton onClick={handleClick}>취소</CancelButton>}
         </Container>
     );
 };
