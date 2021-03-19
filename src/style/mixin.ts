7dae1532b5a502eee7353770955654ee43891f27
GangbacOl <gangyeonjo336@gmail.com>
2021-03-19 20:01:41 +0900

    Add media query mixin

--- src/style/mixin.ts
+++ src/style/mixin.ts
@@ -1,35 +1,70 @@
 import * as theme from '../style/theme';
+import { css, FlattenSimpleInterpolation } from 'styled-components';
 
-export const mobileTablet = (content: string) =>
-    `@media only screen and (max-width:${theme.breakpointTablet}px){
-        ${content}
-    }`;
+export const mobileTablet = (content: FlattenSimpleInterpolation) =>
+    css`
+        @media only screen and (max-width: ${theme.breakpointTabletL}px) {
+            ${content}
+        }
+    `;
 
-export const mobile = (content: string) =>
-    `@media only screen and (max-width: ${theme.breakpointMobile}px) {
-        ${content}
-    }`;
+export const mobile = (content: FlattenSimpleInterpolation) =>
+    css`
+        @media only screen and (max-width: ${theme.breakpointMobile}px) {
+            ${content}
+        }
+    `;
 
-export const tablet = (content: string) =>
-    `@media only screen and (min-width: ${theme.breakpointMobile + 1}px) and (max-width:${
-        theme.breakpointTablet
-    }px) {
-        ${content}
-    }`;
+export const tablet = (content: FlattenSimpleInterpolation) =>
+    css`
+        @media only screen and (min-width: ${theme.breakpointMobile +
+            1}px) and (max-width: ${theme.breakpointTabletL}px) {
+            ${content}
+        }
+    `;
 
-export const desktop = (content: string) =>
-    `@media only screen and (min-width:${theme.breakpointTablet + 1}px) {
-        ${content}
-    }`;
+export const tabletM = (content: FlattenSimpleInterpolation) =>
+    css`
+        @media only screen and (min-width: ${theme.breakpointMobile +
+            1}px) and (max-width: ${theme.breakpointTabletM}px) {
+            ${content}
+        }
+    `;
 
-export const desktopM = (content: string) =>
-    `@media only screen and (min-width:${theme.breakpointTablet + 1}px) and (max-width: ${
-        theme.breakpointDesktopM
-    }px) {
-        ${content}
-    }`;
+export const tabletL = (content: FlattenSimpleInterpolation) =>
+    css`
+        @media only screen and (min-width: ${theme.breakpointTabletM +
+            1}px) and (max-width: ${theme.breakpointTabletL}px) {
+            ${content}
+        }
+    `;
 
-export const desktopL = (content: string) =>
-    `@media only screen and (min-width:${theme.breakpointDesktopM}px) {
-        ${content}
-    }`;
+export const desktop = (content: FlattenSimpleInterpolation) =>
+    css`
+        @media only screen and (min-width: ${theme.breakpointTabletL + 1}px) {
+            ${content}
+        }
+    `;
+
+export const desktopS = (content: FlattenSimpleInterpolation) =>
+    css`
+        @media only screen and (min-width: ${theme.breakpointTabletL +
+            1}px) and (max-width: ${theme.breakpointDesktopS}px) {
+            ${content}
+        }
+    `;
+
+export const desktopM = (content: FlattenSimpleInterpolation) =>
+    css`
+        @media only screen and (min-width: ${theme.breakpointDesktopS +
+            1}px) and (max-width: ${theme.breakpointDesktopM}px) {
+            ${content}
+        }
+    `;
+
+export const desktopL = (content: FlattenSimpleInterpolation) =>
+    css`
+        @media only screen and (min-width: ${theme.breakpointDesktopM + 1}px) {
+            ${content}
+        }
+    `;
